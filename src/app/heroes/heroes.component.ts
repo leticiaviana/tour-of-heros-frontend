7b455a462675ee59afc3ce56f875a91c09d2d96e
leticia <leticia@hotmail.com>
2022-07-19 15:22:21 -0300

    :sparkles: adicionando lista de heroi e interação

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HEROES } from './../mock-heroes';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
 
 @Component({
@@ -6,15 +7,18 @@ import { Hero } from '../hero.model';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
-    name: 'Wanda'
+    name: 'Wanda',
   };
 
-  constructor() { }
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  ngOnInit(): void {
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Ashe'},
+  { id: 2, name: 'Irelia'},
+  { id: 3, name: 'Fiora'},
+  { id: 4, name: 'Lux'},
+  { id: 5, name: 'Tristana'},
+  { id: 6, name: 'VI'},
+  { id: 7, name: 'Caitlyn'},
+  { id: 8, name: 'Xayah'},
+  { id: 9, name: 'Akali'},
+  { id: 10, name: 'Ahri'},
+];
